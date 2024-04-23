0c18a99cc193377c51fea08ac8daf7ba43062d05
Benjaman Tran <t16021999@gmail.com>
2024-04-23 22:42:46 +0700

    set up bg home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,16 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (<div>
-    <p className="font-bold">helo auth</p>
-    <Button variant="ghost" size="lg">click me</Button>
-  </div>
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-sky-800">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          🔒Auth
+        </h1>
+        <p className="text-white text-lg">
+          A simple authentication service
+        </p>
+      </div>
+    </main>
   );
-
 }
