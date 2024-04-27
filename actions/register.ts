121e9ed06d9b4f88c1397a995beed374698c4d34
Benjaman Tran <t16021999@gmail.com>
2024-04-27 15:08:27 +0700

    set up db schema prisma and create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 'use server'
 import { RegisterSchema } from '@/app/schemas'
 import * as z from 'zod'
+import bcrypt from 'bcrypt'
+import { db } from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
@@ -8,6 +11,23 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return { error: 'Invalid fields' }
     }
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const existingUser = await getUserByEmail(email)
 
-    return { success: "Email sent!" }
+    if (existingUser) {
+        return { error: "Email already in use!" }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    // TODO: verification token email
+
+    return { success: "User created!" }
 }
