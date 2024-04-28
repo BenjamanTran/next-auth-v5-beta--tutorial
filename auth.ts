f8727a323c603875da30aa4fb3cc08b9d0ee91a2
Benjaman Tran <t16021999@gmail.com>
2024-04-28 15:54:10 +0700

    finish login/logout

--- auth.ts
+++ auth.ts
@@ -3,11 +3,10 @@ import authConfig from "./auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "./lib/db"
 
-export const { handlers: { GET, POST }, auth } = NextAuth(
-    {
-        adapter: PrismaAdapter(db),
-        session: { strategy: 'jwt' },
-        ...authConfig
-    }
+export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth(
+  {
+    adapter: PrismaAdapter(db),
+    session: { strategy: 'jwt' },
+    ...authConfig
+  }
 )
-
