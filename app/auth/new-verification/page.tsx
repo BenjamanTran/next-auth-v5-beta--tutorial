f40beb72fde2925a6966f0faa9b6229720a29927
Benjaman Tran <t16021999@gmail.com>
2024-05-04 22:26:45 +0700

    setup send email verification

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,10 @@
+import NewVerificationForm from '@/components/auth/new-verifation-form'
+import React from 'react'
+
+const NewVerificationPage = () => {
+  return (
+    <NewVerificationForm/>
+  )
+}
+
+export default NewVerificationPage
---
+++ components/auth/new-verifation-form.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useCallback, useEffect, useState } from "react";
+import { CardWrapper } from "./card-wrapper";
+import { BeatLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
+import { newVerification } from "@/actions/new-verification";
+import { FormSuccess } from "../form-success";
+import { FormError } from "../form-error";
+
+const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    if (success || error) return;
+    if (!token) {
+      setError("Missing Token");
+      return;
+    }
+
+    newVerification(token)
+      .then((data) => {
+        setError(data.error);
+        setSuccess(data.success);
+      })
+      .catch(() => {
+        setError("Something wrong!");
+      });
+  }, [token, success, error]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  return (
+    <CardWrapper
+      headerLabel="Confirming your verification"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <div className="flex items-center w-full justify-center">
+        {!success && !error && <BeatLoader />}
+
+        <FormSuccess message={success} />
+        {!success && <FormError message={error} />}
+      </div>
+    </CardWrapper>
+  );
+};
+
+export default NewVerificationForm;
