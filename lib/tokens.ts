dd9ff28663c058a957451a4a4d3a2b8e38cf9bba
Benjaman Tran <t16021999@gmail.com>
2024-05-12 23:10:14 +0700

    done 50%

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,6 +2,31 @@ import { v4 as uuidv4 } from 'uuid'
 import { db } from './db'
 import { getVerificationTokenByEmail } from '@/data/verification-token'
 import { getPasswordResetByEmail } from '@/data/password-reset-token'
+import crypto from 'crypto'
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token'
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString()
+  //todo: change to 15 minutes
+  const expires = new Date(new Date().getTime() + 3600 * 1000)
+  const existingToken = await getTwoFactorTokenByEmail(email)
+
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id
+      }
+    })
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires
+    }
+  })
+}
 
 export const generateResetPasswordToken = async (email: string) => {
   try {
